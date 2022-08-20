076fffb664bbe08adf0697d8b1f4f2279294452e
bocheng24 <jeffcgp@hotmail.com>
2022-08-20 15:38:36 +0800

    morgan middleware, users routes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const PORT = 3500;
 const app = express();
 
-const toursSimple = `${__dirname}/dev-data/data/tours-simple.json`
+const toursSimple = `${__dirname}/dev-data/data/tours-simple.json`;
 
-app.use(express.json())
+app.use(express.json());
+app.use(morgan('dev'));
 
-const tours = JSON.parse(fs.readFileSync(toursSimple))
+const tours = JSON.parse(fs.readFileSync(toursSimple));
 
 const getAllTours = (requ, res) => {
     res
@@ -88,6 +90,41 @@ const delTourById = (req, res) => {
     })
 }
 
+const getAllUsers = (req, res) => {
+    return res.status(500).json({
+        status: 'Internal Error',
+        message: 'This route has not been defined'
+    })
+}
+
+const getUserById = (req, res) => {
+    return res.status(500).json({
+        status: 'Internal Error',
+        message: 'This route has not been defined'
+    })
+}
+
+const postUser = (req, res) => {
+    return res.status(500).json({
+        status: 'Internal Error',
+        message: 'This route has not been defined'
+    })
+}
+
+const patchUserById = (req, res) => {
+    return res.status(500).json({
+        status: 'Internal Error',
+        message: 'This route has not been defined'
+    })
+}
+
+const delUserById = (req, res) => {
+    return res.status(500).json({
+        status: 'Internal Error',
+        message: 'This route has not been defined'
+    })
+}
+
 app.route('/api/tours')
     .get(getAllTours)
     .post(postTour)
@@ -96,6 +133,15 @@ app.route('/api/tours/:id')
     .get(getTourById)
     .patch(patchTourById)
     .delete(delTourById)
+
+app.route('/api/users')
+    .get(getAllUsers)
+    .post(postUser)
+
+app.route('/api/users/:id')
+    .get(getUserById)
+    .patch(patchUserById)
+    .delete(delUserById)
 
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`)
